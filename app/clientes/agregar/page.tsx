053e972d700c4d0f80dce3438cd2c791ce1bf178
Ignacio Balangero <ignaciobalangero@MacBook-Air-de-Ignacio.local>
2025-04-17 12:39:28 -0300

    🚀 Quick push

--- app/clientes/agregar/page.tsx
+++ app/clientes/agregar/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"; // 👈🏽 esto es clave para evitar el error de build
+
 import AgregarClienteForm from "./AgregarClienteForm";
 
 export default function Page() {
