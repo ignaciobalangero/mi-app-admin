fd12f8c22a9f777a63c93eabedd6387254cace2a
Ignacio Balangero <ignaciobalangero@MacBook-Air.local>
2025-05-12 15:10:13 -0300

    🚀 Quick push

--- app/integracion-sheet/components/FormularioAgregarProducto.tsx
+++ app/integracion-sheet/components/FormularioAgregarProducto.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import { getAuth } from "firebase/auth";
+import { headers } from "next/headers";
+
 
 export default function FormularioAgregarProducto({
   sheetID,
